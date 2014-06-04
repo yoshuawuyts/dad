f2caaf525c5bb703b848b97e49baffa73dbcba06
yoshuawuyts <i@yoshuawuyts.com>
2014-06-04 22:00:18 +0200

    Fix CI test coverage

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/**
+ * Import test statements for CI test coverage.
+ * See: https://github.com/gotwarlost/istanbul/issues/197
+ */
+
+
+require('./application/./application');
+require('./application/attr');
+require('./application/validate');
+
+require('./transactions/add');
+require('./transactions/get');
+require('./transactions/remove');
+require('./transactions/update');
