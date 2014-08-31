95d8cd5804991d4bc03df2b4b6b8dce00285c106
yoshuawuyts <i@yoshuawuyts.com>
2014-08-31 18:51:25 +0200

    ._push: init adapter framework

--- test/_push.js
+++ test/_push.js
@@ -21,20 +21,39 @@ beforeEach(function() {
  */
 
 describe('._push()', function () {
-  it('should catch errors', function (done) {
-    books.on('failure', function(){done()});
-    books._adapters = [function(end) {end('nope')}];
+  it('should catch errors', function() {
+    books._push.bind(books, 123)
+      .should.throw('Method should be a string');
 
-    books._push();
+    books._push.bind(books, 'hello')
+      .should.throw('Data should not be undefined');
   });
 
-  it('should call the adapters', function (done) {
-    books.on('success', function() {done()});
-    books._adapters = [
-      function(end) {setTimeout(function(){end()}, 300);},
-      function(end) {end()}
-    ];
+  it('should call a single adapter', function(done) {
+    books.on('sync', function() {done()});
 
-    books._push();
+    books._adapters = [{
+      create: function(data, supply, cb) {
+        supply.next(data, supply, cb);
+      }
+    }];
+
+    books._push('create', {cat: 'fish'});
+  });
+
+  it('should chain call multiple adapters', function(done) {
+    books.on('sync', function() {done()});
+
+    books._adapters = [{
+      create: function(data, supply, cb) {
+        supply.next(data, supply, cb);
+      }
+    }, {
+      create: function(data, supply, cb) {
+        supply.next(data, supply, cb);
+      }
+    }];
+
+    books._push('create', {cat: 'fish'});
   });
 });
